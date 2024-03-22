d954aaa277d68f45dfc0dc59560af39636ed57b2
Nicolas Hurtubise <316k@legtux.org>
2024-03-22 11:20:31 -0400

    Renomme les constantes

--- Assets/Scripts/ConstantesJeu.cs
+++ Assets/Scripts/ConstantesJeu.cs
@@ -2,16 +2,16 @@ public static class ConstantesJeu
 {
     // Ça représente la quantité d'énergie que le joueur perd à chaque minute
     // dans le jeu
-    public const float MARCHER = 0.001f;
-    public const float COURIR = 0.005f;
-    public const float PLANTER = 0.002f;
-    public const float CUEILLIR = 0.001f;
-    public const float DORMIR = -0.005f;    // Le joueur gagne de l'énergie en dormant
-    public const float IMMOBILE = 0.0001f;   
+    public const float COUT_MARCHER = 0.001f;
+    public const float COUT_COURIR = 0.005f;
+    public const float COUT_PLANTER = 0.002f;
+    public const float COUT_CUEILLIR = 0.001f;
+    public const float COUT_IMMOBILE = 0.0001f;
 
     // Ce que le joueur peut gagner en énergie
-    public const float MANGER_OEUF = 0.25f;
-    public const float MANGER_CHOU = 0.35f;
+    public const float GAIN_ENERGIE_MANGER_OEUF = 0.25f;
+    public const float GAIN_ENERGIE_MANGER_CHOU = 0.35f;
+    public const float GAIN_ENERGIE_SOMMEIL = -0.005f;
     
     // Constantes relatives au temps dans une journée
     public const float MINUTES_PAR_JOUR = 1440.0f;
